0a5609116386d4aad4887b84fc84bc9ff9cbce60
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-02-12 09:29:36 +0100

    feat: update tailwind configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,11 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-      },
+        gradient: "radial-gradient(152.84% 95.20% at -576.37% 1937.13%, rgba(35.85, 160.11, 181.49, 0.20) 0%, rgba(36, 160, 181, 0) 100%)",
+        border: '#0E464F',
+        'border2': '#2BA4B9',
+        'green-1' : '#041E23'
+      }
     },
   },
   plugins: [],
