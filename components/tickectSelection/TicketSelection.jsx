5e47c6595b9f5dbdd075aa22b37faa18a057b731
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-02-12 10:23:54 +0100

    feat: updated event page layout

--- components/tickectSelection/TicketSelection.jsx
+++ components/tickectSelection/TicketSelection.jsx
@@ -1,11 +1,23 @@
+'use client'
 import React from 'react'
-import TicketDetails from './TicketDetails'
+import EventBanner from './EventBanner'
+import Divider from '../ui/Divider'
+import TicketType from './TicketType'
+import NoOfTicket from './NoOfTicket'
+import ButtonPair from '../layout/ButtonPair'
+import { useRouter } from 'next/navigation'
 
 const TicketSelection = ({}) => {
+  const route = useRouter();
+  
   return (
-    <div className=''>
-        <TicketDetails/>
-    </div>
+    <>
+      <EventBanner/>
+      <Divider/>
+      <TicketType/>
+      <NoOfTicket/>
+      <ButtonPair text1={'Cancel'} text2={'Next'} onClick_2={()=>{route.push("/event/attendee-details")}} className='md:border border-border md:bg-green-1 gap-4 md:gap-8 md:rounded-full md:px-12'/>
+    </>
   )
 }
 
