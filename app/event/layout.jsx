13e39100ead8867ccb3c2f2d9b58e13ed03e3fc6
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-02-12 21:49:09 +0100

    feat: added gradient background

--- app/event/layout.jsx
+++ app/event/layout.jsx
@@ -6,6 +6,7 @@ export default function Layout({ children }) {
   return (
     <EventLayout>
         {children}
+        <div className="absolute -z-50 inset-0 bg-[radial-gradient(52.52%_32.71%_at_50%_97.66%,rgba(36,160,181,0.2)_0%,rgba(36,160,181,0)_100%)]"></div>
     </EventLayout>
   );
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -7,10 +7,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${roboto.className} antialiased p-5 lg:px-[120px] lg:py-6`}
-      > 
-      <Nav/>
-            {children}
+        className={`${roboto.className} antialiased p-5 lg:px-[120px] lg:pt-6 pb-28 relative`}
+      >
+        <Nav/>
+        {children}
       </body>
     </html>
   );
