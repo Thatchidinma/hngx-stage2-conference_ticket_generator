6233b98291d47eca55b71b70cf7d11ccc1ba0899
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-02-13 13:24:15 +0100

    fix: fixed vercel deployment error during Next.js build due to the use of localStorage outside a useEffect

--- components/readyTicket/ReadyTicketdetails.jsx
+++ components/readyTicket/ReadyTicketdetails.jsx
@@ -1,18 +1,30 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import ButtonPair from '../layout/ButtonPair'
 import { useRouter } from 'next/navigation'
 import Ticket from './Ticket'
 
 const ReadyTicketdetails = () => {
-    const route = useRouter(); 
+    const route = useRouter();
+    
+    useEffect(()=>{
+      const downloadTicket = () => { 
+        localStorage.clear()
+      }
+
+      const bookNew = () => {
+        route.push("/event"), 
+        localStorage.clear()
+      }
+    })
+    
     
   return (
     <section className='slide-up-now mt-8'>
       <h2 className="text-3xl font-bold text-center">Your Ticket is Booked!</h2>
       <p className='text-center m-6'>Check your email for a copy or you can download</p>
       <Ticket/>
-        <ButtonPair text1={'Book Another Ticket'} text2={'Download Ticket'} onClick_1={()=>{route.push("/event"), localStorage.clear()}} onClick_2={()=>{localStorage.clear();}} className='gap-4'/>
+        <ButtonPair text1={'Book Another Ticket'} text2={'Download Ticket'} onClick_1={()=>{bookNew()}} onClick_2={()=>{downloadTicket()}} className='gap-4'/>
     </section>
   )
 }
