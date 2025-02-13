2d957fc9a0c2647948b98769755478c176061f91
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-02-13 14:33:21 +0100

    fix: used local storage

--- components/attendeeDetails/AttendeeForm.jsx
+++ components/attendeeDetails/AttendeeForm.jsx
@@ -23,17 +23,17 @@ const AttendeeForm = () => {
 
   const {register, handleSubmit,watch, formState:{errors}} = useForm({resolver: zodResolver(schema)})
 
-  // const nameValue = watch("name");
-  // const emailValue = watch("email");
-  // const messageValue = watch("specialRequest");
+  const nameValue = watch("name");
+  const emailValue = watch("email");
+  const messageValue = watch("specialRequest");
 
-  // useEffect(()=>{
-  //   if (typeof window !== 'undefined') {
-  //   localStorage.setItem('name', nameValue);
-  //   localStorage.setItem('email', emailValue);
-  //   localStorage.setItem('request', messageValue);
-  //   }
-  // },[nameValue,emailValue, messageValue])
+  useEffect(()=>{
+    if (typeof window !== 'undefined') {
+    localStorage.setItem('name', nameValue);
+    localStorage.setItem('email', emailValue);
+    localStorage.setItem('request', messageValue);
+    }
+  },[nameValue,emailValue, messageValue])
 
 
   const submitData = (data) =>{
