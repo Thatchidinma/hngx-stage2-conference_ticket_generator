3b5d2bf09ae69201bda48f2644ebfdaee8ec7628
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-02-13 13:31:14 +0100

    fix: fixed vercel deployment error

--- components/readyTicket/ReadyTicketdetails.jsx
+++ components/readyTicket/ReadyTicketdetails.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import ButtonPair from '../layout/ButtonPair'
 import { useRouter } from 'next/navigation'
 import Ticket from './Ticket'
@@ -7,24 +7,12 @@ import Ticket from './Ticket'
 const ReadyTicketdetails = () => {
     const route = useRouter();
     
-    useEffect(()=>{
-      const downloadTicket = () => { 
-        localStorage.clear()
-      }
-
-      const bookNew = () => {
-        route.push("/event"), 
-        localStorage.clear()
-      }
-    })
-    
-    
   return (
     <section className='slide-up-now mt-8'>
       <h2 className="text-3xl font-bold text-center">Your Ticket is Booked!</h2>
       <p className='text-center m-6'>Check your email for a copy or you can download</p>
       <Ticket/>
-        <ButtonPair text1={'Book Another Ticket'} text2={'Download Ticket'} onClick_1={()=>{bookNew()}} onClick_2={()=>{downloadTicket()}} className='gap-4'/>
+        <ButtonPair text1={'Book Another Ticket'} text2={'Download Ticket'} onClick_1={()=>{route.push("/event")}}  className='gap-4'/>
     </section>
   )
 }
