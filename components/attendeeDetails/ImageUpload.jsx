add4e43f6673faa68578a065bea1ac4c1f65a1e6
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-02-13 18:26:31 +0100

    feat: installed react dropbox and implemented drag and drop image component

--- components/attendeeDetails/ImageUpload.jsx
+++ components/attendeeDetails/ImageUpload.jsx
@@ -1,16 +1,58 @@
 import Upload from '@/icons/Upload'
-import React from 'react'
+import Image from 'next/image'
+import React, { useEffect } from 'react'
+import { useCallback, useState } from "react"
+import { useDropzone } from "react-dropzone"
 
 const ImageUpload = () => {
+  const [dataURL, setDataURL] = useState(null)
+  // const [uploadedURL, setUploadedURL] = useState(null)
+
+  const onDrop = useCallback(acceptedFiles => {
+    acceptedFiles.forEach(file => {
+      const reader = new FileReader()
+      reader.onabort = () => console.log("file reading was aborted")
+      reader.onerror = () => console.log("file reading has failed")
+      reader.onload = () => {
+        const binaryStr = reader.result
+        setDataURL(binaryStr)
+      }
+      reader.readAsDataURL(file)
+    })
+  }, [])
+
+  const {
+    getRootProps,
+    acceptedFiles,
+    getInputProps,
+    isDragActive,
+  } = useDropzone({ onDrop })
+
+  const selectedFile = acceptedFiles[0]
+
+  useEffect(()=>{
+    console.log(selectedFile)
+  },[selectedFile])
+
   return (
-    <div className='border border-border p-6 rounded-3xl bg-[#052228] '>
+    <div className='border border-border p-6 rounded-3xl bg-[#052228] cursor-pointer' {...getRootProps()}>
+      <input  {...getInputProps()} />
         <p className="">Upload Profile Photo</p>
         <div className="relative w-full flex">
-        <input id='image' type="file" accept="image/png, image/gif, image/jpeg" className='hidden'></input>
-        <label htmlFor='image' className="z-10 w-60 h-60 border-4 bg-[#0E464F] border-border2 rounded-[32px] mx-auto mt-3 mb-1 flex flex-col justify-center items-center text-center gap-5">
+        <div className="overflow-hidden relative z-10 w-60 h-60 border-4 bg-[#0E464F] border-border2 rounded-[32px] mx-auto mt-3 mb-1 flex flex-col justify-center items-center text-center gap-5">
+          {dataURL && 
+          <Image
+          src={dataURL}
+          alt='upload'
+          width={240}
+          height={240}
+          className='z-10 hover:z-0 w-full bg-inherit'
+          />}
+            <div className={`${dataURL ? 'hover:z-10 bg-black/20' : '' } w-60 h-60  absolute flex flex-col items-center justify-center`}>
             <Upload/>
             <p className="">Drag & drop or click to <br/>upload</p>
-        </label>
+            </div>
+        </div>
         <div className="hidden sm:block top-8 absolute w-full h-[200px] bg-[#041B20]"></div>
         </div>
     </div>
