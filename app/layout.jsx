31f066a73a14dff0b0977ceada36f98cf0faeb8d
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-02-14 09:41:55 +0100

    fix: added favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,11 +2,15 @@ import { TicketDetailsProvider } from "@/context/TickectDetailsContext";
 import { roboto } from "./font";
 import "./globals.css";
 import Nav from "@/components/layout/Nav";
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
   
   return (
     <html lang="en">
+      <Head>
+      <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body
         className={`${roboto.className} antialiased p-5 md:px-[120px] md:pt-6 pb-28 relative min-h-screen`}
       >
